149ee90958cbfa62da397d3683ae9709af53f05d
Encounter27 <pradip.phab27@gmail.com>
2016-08-04 01:24:05 +0530

    Adding trie data structures.

---
+++ AdvanceDataStructures/DsProblems/run.sh
@@ -0,0 +1,5 @@
+make INPUT=Input SRC=BstProb.cpp
+make clean
+
+# delete swp files
+rm -rf  .*.swp   *~      ../DsUtils/.*.swp    ../DsUtils/*~
---
+++ AdvanceDataStructures/DsUtils/Trie.h
@@ -0,0 +1,24 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<string>
+
+#define ALPHABET_SIZE 26
+
+struct TrieNode
+{
+	TrieNode* children[ALPHABET_SIZE];
+	bool endOfWord;	
+};
+
+TrieNode* CreateTrie();
+void Insert(TrieNode* root, const string key);
+bool Search(TrieNode* root, const string key);
+bool PrefixSearch(TrieNode* root, const string prefix);
+void Delete(TrieNode* root, const string key);
+
+
+#endif
